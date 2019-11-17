41ccfbe4e204814a03163b3c578c3b958fb31232
unknown <arne.n.bjerkaas@gmail.com>
2019-11-17 12:34:52 +0100

    Search for news topic

--- src/Components/Article/ArticleDetail.js
+++ src/Components/Article/ArticleDetail.js
@@ -1,26 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 function ArticleDetail(props) {
-  /*   const [item, setItem] = useState([]);
-
-  useEffect(() => {
-    fetchItem();
-  }, []);
-
-  const url =
-    "https://newsapi.org/v2/top-headlines?" +
-    "sources=bbc-news&" +
-    "apiKey=4355957195de4294ad512147dbb0167a";
-
-  const fetchItem = async () => {
-    const data = await fetch(url);
-    const item = await data.json();
-    setItem(item.articles[props.articleIndex]);
-    console.log(item.articles[props.articleIndex]);
-  };
-
-  console.log(`Article nr. ${props.articleIndex}`, item);
- */
   return (
     <div>
       {props.article !== undefined ? (
--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Articles(props) {
   const [items, setItems] = useState([]);
-  const [indexState, setIndexState] = useState(undefined);
+  const [topic, setTopic] = useState("");
 
   useEffect(() => {
     fetchArticles();
@@ -29,15 +29,40 @@ function Articles(props) {
     getArticleIndex();
   }, []);
 
-  // Function for getting the index from map
+  // Callback-function for getting the index and correct article from map
   const getArticleIndex = (item, index) => {
-    // Parent callback function
     props.indexCallback(item, index);
+  };
+
+  const onTopicChangeHandler = e => {
+    setTopic(e.target.value);
+  };
+
+  const getTopicHandler = () => {
+    const url =
+      "https://newsapi.org/v2/top-headlines?" +
+      `q=${topic}&` +
+      "pageSize=10&" +
+      "apiKey=4355957195de4294ad512147dbb0167a";
+    fetch(url)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data.articles);
+        setItems(data.articles);
+      });
   };
 
   return (
     <div style={css.articlesContainer}>
-      <h1 style={css.title}>Articles</h1>
+      <h1 style={css.title}>Top Stories Right Now</h1>
+      <div style={css.topicContainer}>
+        <div>News Topic: </div>
+        <input onChange={onTopicChangeHandler} type="text" />
+        <button onClick={getTopicHandler}>Search</button>
+      </div>
+      <label />
       {items.map((item, index) => (
         <div style={css.itemContainer}>
           <img src={item.urlToImage} alt="Image" style={css.articleImage} />
@@ -66,6 +91,13 @@ const css = {
   title: {
     width: "100%",
     textAlign: "center"
+  },
+  topicContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    margin: "20px 0"
   },
   articlesContainer: {
     display: "flex",
